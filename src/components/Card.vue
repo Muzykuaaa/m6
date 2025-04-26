f4b3a80cbc60db8f27f7fd66a7f4caaee46b5314
Muzykuaaa <nnen15100@gmail.com>
2025-04-26 18:10:56 +0200

    .

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  item: Object,
+});
+</script>
+<template>
+  <a>
+    {{ item }}
+    <img :src="item.img" alt="" />
+    <h1>{{ item.title }}</h1>
+    <p>{{ item.desc }}</p>
+    <!-- <i v-if="item.like" class="fas fa-star"></i>
+    <i v-else class="far fa-star"></i> -->
+    <i class="fas fa-star" :class="item.like ? 'red' : 'black'"></i>
+  </a>
+</template>
+
+<style scoped>
+a {
+  width: 500px;
+  padding: 10px;
+  background: #32383e;
+}
+.red {
+    color: red;
+}
+.black{
+    color: black;
+}
+</style>
+
+
---
+++ src/views/Main.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Card from "../components/Card.vue";
+import { ref } from "vue";
+
+let count = ref(8)
+
+
+let data = [
+    {
+        img: "ссылка",
+        title: "назв",
+        desc: "Опис",
+        like: true
+    },
+        {
+        img: "ссылка",
+        title: "назв1",
+        desc: "Опис1",
+        like: false
+    }
+]
+
+setInterval(() => {
+    count.value++;
+}, 500);
+</script>
+<template>
+  <section>
+    {{ count }}
+    <Card v-for="(item, index) in data" :key="index" :item="item"/>
+
+  </section>
+</template>
+
+<style scoped></style>
